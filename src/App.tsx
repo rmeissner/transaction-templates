03248dfd5169147601d91981d89d392a4c816a3b
Richard Meissner <msc.meissner@gmail.com>
2021-02-26 01:02:27 +0100

    Use file upload

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import { ThemeProvider } from '@material-ui/styles'
 import blue from '@material-ui/core/colors/blue'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import txTemplate from './templates/compound'
-import { InputId } from './model/templates'
+import { InteractionTemplate, InputId } from './model/templates'
 import { buildTemplate, GeneratedTx } from './utils/encoding'
 import { ethers } from 'ethers'
 import { useSafeAppsSDK } from '@gnosis.pm/safe-apps-react-sdk';
 import { checkedTx } from './utils/sapp'
+import template1 from './templates/compound'
+import template2 from './templates/uniswap'
 
 declare global {
   interface Window { ethereum: any; }
@@ -37,9 +38,27 @@ const App = () => {
   const [generatedTxs, setGeneratedTxs] = useState<GeneratedTx[]>([])
   const [userInputs, setUserInputs] = useState<Record<InputId, string>>({})
   const { sdk, connected } = useSafeAppsSDK();
-  const template = txTemplate
+  const [template, setTemplate] = useState<InteractionTemplate | undefined>(undefined)
+  console.log(JSON.stringify(template1))
+  console.log(JSON.stringify(template2))
+  
+  const handleFileUpload = React.useCallback(async (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      const file = e.target.files!![0]
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        if (!e.target?.result) return;
+        const template = JSON.parse(e.target.result.toString())
+        setTemplate(template)
+      }
+      reader.readAsBinaryString(file)
+    } catch (e) {
+      console.error(e)
+    }
+  }, [setTemplate])
 
   const build = useCallback(async () => {
+    if (!template) return
     try {
       const txs = buildTemplate(template, userInputs)
       console.log({ txs })
@@ -52,7 +71,7 @@ const App = () => {
   const execute = useCallback(async (tx: GeneratedTx) => {
     try {
       if (connected) {
-        sdk.txs.send({ txs: [ checkedTx(tx) ] })
+        sdk.txs.send({ txs: [checkedTx(tx)] })
         return
       }
       await window.ethereum.enable()
@@ -87,33 +106,36 @@ const App = () => {
       <CssBaseline />
       <div className={classes.content}>
         <h1>
-          Transaction template: {template.name}
+          Transaction template: {template?.name || "Upload Template"}
         </h1>
-        <h3>Inputs</h3>
-        {Object.entries(template.inputs).map(([id, input]) => {
-          console.log(input)
-          switch (input.details.type) {
-            case "bn":
-            case "json":
-              return (
-                <TextField
-                  placeholder={input.details.hint}
-                  label={input.label}
-                  value={userInputs[id]}
-                  onChange={(e) => { setUserInputs(updateUserInputs(userInputs, id, e.target.value)) }}
-                  className={classes.input} />
-              )
-          }
-        })}
-        <Button color="primary" onClick={build}>Build</Button>
-        <h3>Transactions {connected && (<Button color="primary" onClick={() => executeAll(generatedTxs)}>Execute All</Button>)}</h3>
-        {generatedTxs.map((tx, index) => (
-          <>
-            {tx.description || `Transaction #${index + 1}`}
-            <Button color="primary" onClick={() => execute(tx)}>Execute</Button>
-            <br />
-          </>
-        ))}
+        <input type="file" accept=".json" onChange={handleFileUpload} /><br />
+        {template && (<>
+          <h3>Inputs</h3>
+          {Object.entries(template.inputs).map(([id, input]) => {
+            console.log(input)
+            switch (input.details.type) {
+              case "bn":
+              case "json":
+                return (
+                  <TextField
+                    placeholder={input.details.hint}
+                    label={input.label}
+                    value={userInputs[id]}
+                    onChange={(e) => { setUserInputs(updateUserInputs(userInputs, id, e.target.value)) }}
+                    className={classes.input} />
+                )
+            }
+          })}
+          <Button color="primary" onClick={build}>Build</Button>
+          <h3>Transactions {connected && (<Button color="primary" onClick={() => executeAll(generatedTxs)}>Execute All</Button>)}</h3>
+          {generatedTxs.map((tx, index) => (
+            <>
+              {tx.description || `Transaction #${index + 1}`}
+              <Button color="primary" onClick={() => execute(tx)}>Execute</Button>
+              <br />
+            </>
+          ))}
+        </>)}
       </div>
     </ThemeProvider>
   );
